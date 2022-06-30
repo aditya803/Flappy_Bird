fda8a18789e25e59f48a72f59bb511a710dfe484
aditya803 <adityapan25@gmail.com>
2022-07-01 02:28:42 +0530

    UI UPDATE1

--- lib/Pages/HomePage.dart
+++ lib/Pages/HomePage.dart
@@ -1,5 +1,7 @@
 import 'dart:async';
-import 'dart:ffi';
+import 'dart:ui';
+// import 'dart:ffi';
+// import 'dart:html';
 
 import 'package:flappy_bird/Pages/bird.dart';
 import 'package:flutter/material.dart';
@@ -13,22 +15,35 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   
-  double birdYaxis = 0;
+  static double birdYaxis = 0;
   double time =0;
   double height =0;
-  double initialHeight=0;
+  double initialHeight= birdYaxis;
+  bool gameState = false;
 
   void jump(){
-    initialHeight = birdYaxis;
-    Timer.periodic(Duration(milliseconds: 100), (timer){
-      time += 0.1;
-      height = -4.9 * time * time + 2.9 * time;
+    setState(() {
+      initialHeight = birdYaxis;
+      time = 0;
+    });
+  }
+
+  void startGame(){
+    gameState = true;
+    Timer.periodic(Duration(milliseconds: 60), (timer){
+      time += 0.05;
+      height = -4.9 * time * time + 2.8 * time;
       setState(() {
         birdYaxis = initialHeight - height;
       });
+
+      if(birdYaxis>1){
+        timer.cancel();
+        gameState = false;
+      }
+
     });
   }
-  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -37,7 +52,14 @@ class _HomePageState extends State<HomePage> {
           Expanded(
             flex: 2,
               child: GestureDetector(
-                onTap: jump,
+                onTap: (){
+                  if(gameState){
+                    jump();
+                  }
+                  else{
+                    startGame();
+                  }
+                },
                 child: AnimatedContainer(
                   alignment: Alignment(0,birdYaxis),
                   color: Colors.blue,
@@ -46,9 +68,34 @@ class _HomePageState extends State<HomePage> {
                 ),
               )
           ),
+          Container(
+            height: 15,
+            color: Colors.green,
+          ),
           Expanded(
               child: Container(
-                color: Colors.green,
+                color: Colors.brown,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                      children:[
+                        Text("SCORE",style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold, fontSize: 20 ),),
+                        SizedBox(height: 10),
+                        Text("0",style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold, fontSize: 20 ))
+                      ]
+                    ),
+                    Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                      children:[
+                        Text("HIGHSCORE",style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,  fontSize: 20 )),
+                        SizedBox(height: 10),
+                        Text("0",style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,  fontSize: 20 ))
+                      ]
+                    )
+                  ],
+                ),
               )
           ),
         ],
