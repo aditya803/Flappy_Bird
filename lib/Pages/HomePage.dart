3b6ae520137308da31a14dc28e3d203b05f7d57d
aditya803 <adityapan25@gmail.com>
2022-07-03 01:56:13 +0530

    full loop

--- lib/Pages/HomePage.dart
+++ lib/Pages/HomePage.dart
@@ -21,8 +21,8 @@ class _HomePageState extends State<HomePage> {
   double height =0;
   double initialHeight= birdYaxis;
   bool gameState = false;
-  double pos1 = 0;
-  double pos2 = 1;
+  static double pos1 = 0;
+  double pos2 = pos1 + 1.5;
 
   void jump(){
     setState(() {
@@ -38,8 +38,19 @@ class _HomePageState extends State<HomePage> {
       height = -4.9 * time * time + 2.8 * time;
       setState(() {
         birdYaxis = initialHeight - height;
-        pos1 -= 0.05;
-        pos2 -= 0.05;
+      });
+
+      setState(() {
+        if(pos1 < -2)
+          {pos1 +=3.5;}
+        else
+          {pos1 -= 0.05;}
+      });
+      setState(() {
+        if(pos2 < -2)
+          {pos2 +=3.5;}
+        else
+          {pos2 -= 0.05;}
       });
 
       if(birdYaxis>1){
@@ -51,101 +62,101 @@ class _HomePageState extends State<HomePage> {
   }
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: [
-          Expanded(
-            flex: 2,
-              child: Stack(
-                children: [
-                  GestureDetector(
-                    onTap: (){
-                      if(gameState){
-                        jump();
-                      }
-                      else{
-                        startGame();
-                      }
-                    },
-                    child: AnimatedContainer(
+    return GestureDetector(
+      onTap: (){
+        if(gameState){
+          jump();
+        }
+        else{
+          startGame();
+        }
+      },
+      child: Scaffold(
+        body: Column(
+          children: [
+            Expanded(
+              flex: 2,
+                child: Stack(
+                  children: [
+                    AnimatedContainer(
                       alignment: Alignment(0,birdYaxis),
                       color: Colors.blue,
                       duration: Duration(milliseconds: 0),
                       child: Mybird(),
                     ),
-                  ),
-                  Container(
-                    alignment: Alignment(0,-0.2),
-                    child: gameState
-                        ? Text(" ")
-                        : Text("T A P  T O  P L A Y",
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 20)
-                    )
-                  ),
-                  AnimatedContainer(
-                    duration: Duration(milliseconds: 0),
-                    alignment: Alignment(pos1,1.1),
-                    child: MyObject(
-                      size: 200.0,
+                    Container(
+                      alignment: Alignment(0,-0.2),
+                      child: gameState
+                          ? Text(" ")
+                          : Text("T A P  T O  P L A Y",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 20)
+                      )
                     ),
-                  ),
-                  AnimatedContainer(
-                    duration: Duration(milliseconds: 0),
-                    alignment: Alignment(pos1,-1.1),
-                    child: MyObject(
-                      size: 200.0,
+                    AnimatedContainer(
+                      duration: Duration(milliseconds: 0),
+                      alignment: Alignment(pos1,1.1),
+                      child: MyObject(
+                        size: 200.0,
+                      ),
                     ),
-                  ),
-                  AnimatedContainer(
-                    duration: Duration(milliseconds: 0),
-                    alignment: Alignment(pos2,1.1),
-                    child: MyObject(
-                      size: 120.0,
-                    )
-                  ),
-                  AnimatedContainer(
-                    duration: Duration(milliseconds: 0),
-                    alignment: Alignment(pos2,-1.1),
-                    child: MyObject(
-                      size: 150.0,
+                    AnimatedContainer(
+                      duration: Duration(milliseconds: 0),
+                      alignment: Alignment(pos1,-1.1),
+                      child: MyObject(
+                        size: 200.0,
+                      ),
                     ),
-                  ),
-                ],
-              )
-          ),
-          Container(
-            height: 15,
-            color: Colors.green,
-          ),
-          Expanded(
-              child: Container(
-                color: Colors.brown,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                      children:[
-                        Text("SCORE",style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold, fontSize: 20 ),),
-                        SizedBox(height: 10),
-                        Text("0",style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold, fontSize: 20 ))
-                      ]
+                    AnimatedContainer(
+                      duration: Duration(milliseconds: 0),
+                      alignment: Alignment(pos2,1.1),
+                      child: MyObject(
+                        size: 120.0,
+                      )
                     ),
-                    Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                      children:[
-                        Text("HIGHSCORE",style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,  fontSize: 20 )),
-                        SizedBox(height: 10),
-                        Text("0",style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,  fontSize: 20 ))
-                      ]
-                    )
+                    AnimatedContainer(
+                      duration: Duration(milliseconds: 0),
+                      alignment: Alignment(pos2,-1.1),
+                      child: MyObject(
+                        size: 150.0,
+                      ),
+                    ),
                   ],
-                ),
-              )
-          ),
-        ],
+                )
+            ),
+            Container(
+              height: 15,
+              color: Colors.green,
+            ),
+            Expanded(
+                child: Container(
+                  color: Colors.brown,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                        children:[
+                          Text("SCORE",style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold, fontSize: 20 ),),
+                          SizedBox(height: 10),
+                          Text("0",style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold, fontSize: 20 ))
+                        ]
+                      ),
+                      Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                        children:[
+                          Text("HIGHSCORE",style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,  fontSize: 20 )),
+                          SizedBox(height: 10),
+                          Text("0",style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,  fontSize: 20 ))
+                        ]
+                      )
+                    ],
+                  ),
+                )
+            ),
+          ],
+        ),
       ),
     );
   }
